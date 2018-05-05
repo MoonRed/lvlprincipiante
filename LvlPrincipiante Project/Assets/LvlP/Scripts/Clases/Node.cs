7d4b3eb1dde13d5eb418e5ce575166897ebb8c43
Moon Antonio <pinchodev91@gmail.com>
2018-05-05 17:08:28 +0200

    Update motor

---
+++ LvlPrincipiante Project/Assets/LvlP/Scripts/Clases/Node.cs
@@ -0,0 +1,102 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Node.cs (02/05/2018)															\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Clase base del nodo.										\\
+// Fecha Mod:		02/05/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using LvlP.SubSistemas;
+#endregion
+
+namespace LvlP.Clases
+{
+	/// <summary>
+	/// <para>Clase base del nodo.</para>
+	/// </summary>
+	public class Node : MonoBehaviour 
+	{
+		#region Variables Publicas
+		/// <summary>
+		/// <para>Color cuando se ha presionado.</para>
+		/// </summary>
+		public Color colorPresionado;							// Color cuando se ha presionado
+		/// <summary>
+		/// <para>Pequeña desviacion que se le quiera dar a la torre.</para>
+		/// </summary>
+		public Vector3 offSet;                                  // Pequeña desviacion que se le quiera dar a la torre
+		#endregion
+
+		#region Variables Privadas
+		/// <summary>
+		/// <para>Root donde estan todas las torres.</para>
+		/// </summary>
+		private Transform rootTorres;							// Root donde estan todas las torres
+		/// <summary>
+		/// <para>Torre instanciada (Solo la ultima).</para>
+		/// </summary>
+		private GameObject torre;								// Torre instanciada (Solo la ultima)
+		/// <summary>
+		/// <para>Componente renderer del nodo.</para>
+		/// </summary>
+		private Renderer rend;									// Componente renderer del nodo
+		/// <summary>
+		/// <para>Color inicial del nodo.</para>
+		/// </summary>
+		private Color colorInicial;                             // Color inicial del nodo
+		#endregion
+
+		#region Inicializadores
+		/// <summary>
+		/// <para>Cargador de <see cref="Node"/>.</para>
+		/// </summary>
+		private void Start()// Cargador de Node
+		{
+			// Asignar todas las variables necesarias
+			rend = GetComponent<Renderer>();
+			colorInicial = rend.material.color;
+			rootTorres = GameObject.Find("Torres").transform;
+		}
+		#endregion
+
+		#region Eventos
+		/// <summary>
+		/// <para>Cuando el mouse es presionado.</para>
+		/// </summary>
+		private void OnMouseDown()// Cuando el mouse es presionado.
+		{
+			// Comprobamos que el nodo no tenga una torre ya
+			if (torre != null)
+			{
+				return;
+			}
+
+			// Obtenemos la torre para instanciarla y posteriormente colocarla hija del root de torres
+			GameObject torreParaInstanciar = ManagerBuild.instance.GetTorre();
+			torre = (GameObject)Instantiate(torreParaInstanciar, transform.position + offSet, transform.rotation);
+			torre.transform.parent = rootTorres;
+		}
+
+		/// <summary>
+		/// <para>Cuando el mouse es clicado.</para>
+		/// </summary>
+		private void OnMouseEnter()// Cuando el mouse es clicado
+		{
+			// Cambiar el color
+			rend.material.color = colorPresionado;
+		}
+
+		/// <summary>
+		/// <para>Cuando el mouse deja de ser clicado.</para>
+		/// </summary>
+		private void OnMouseExit()// Cuando el mouse deja de ser clicado
+		{
+			// Cambiar el color
+			rend.material.color = colorInicial;
+		}
+		#endregion
+	}
+}
---
+++ LvlPrincipiante Project/Assets/LvlP/Scripts/SubSistemas/ManagerBuild.cs
@@ -0,0 +1,77 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// ManagerBuild.cs (02/05/2018)													\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Manager de la construccion de torres.						\\
+// Fecha Mod:		02/05/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace LvlP.SubSistemas
+{
+	/// <summary>
+	/// <para>Manager de la construccion de torres.</para>
+	/// </summary>
+	public class ManagerBuild : MonoBehaviour 
+	{
+		#region Singleton
+		/// <summary>
+		/// <para>Instancia de <see cref="ManagerBuild"/>.</para>
+		/// </summary>
+		public static ManagerBuild instance;                // Instancia de ManagerBuild
+		#endregion
+
+		#region Variables Publicas
+		/// <summary>
+		/// <para>Prefab de la torre para instanciar.</para>
+		/// </summary>
+		public GameObject torrePrefab;                      // Prefab de la torre para instanciar
+		#endregion
+
+		#region Variables Privadas
+		/// <summary>
+		/// <para>Siguiente torre para construir.</para>
+		/// </summary>
+		private GameObject torreAConstruir;                 // Siguiente torre para construir
+		#endregion
+
+		#region Inicializadores
+		/// <summary>
+		/// <para>Inicializador de <see cref="ManagerBuild"/>.</para>
+		/// </summary>
+		private void Awake()// Inicializador de ManagerBuild
+		{
+			// Singleton simple
+			if (instance != null)
+			{
+				return;
+			}
+			instance = this;
+		}
+
+		/// <summary>
+		/// <para>Cargador de <see cref="ManagerBuild"/>.</para>
+		/// </summary>
+		private void Start()// Cargador de ManagerBuild
+		{
+			// Nueva torre para construir
+			torreAConstruir = torrePrefab;
+		}
+		#endregion
+
+		#region Funcionalidad
+		/// <summary>
+		/// <para>Obtiene la torre para construirla.</para>
+		/// </summary>
+		/// <returns></returns>
+		public GameObject GetTorre()// Obtiene la torre para construirla
+		{
+			return torreAConstruir;
+		}
+		#endregion
+	}
+}
